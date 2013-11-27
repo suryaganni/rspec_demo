f79f665a57d3f6475f1718d06079807e59d2622d
suryag <suryag@surya-g.(none)>
2013-11-27 16:24:48 +0530

    corrected model spec

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,19 +3,19 @@ require 'spec_helper'
 describe Task do
 
   it "with name presence" do
-    FactoryGirl.create(:task, :user => FactoryGirl.create(:user)).should be_valid
+    FactoryGirl.build(:user, :tasks => [FactoryGirl.build(:task)]).should be_valid
   end
 
   it "with name absence" do
-    FactoryGirl.build(:task, :user => FactoryGirl.create(:user), :name => "").should_not be_valid
+    FactoryGirl.build(:user, :tasks => [FactoryGirl.build(:task, :name => "")]).should_not be_valid
   end
 
   it "with content presence" do
-    FactoryGirl.create(:task, :user => FactoryGirl.create(:user)).should be_valid
+    FactoryGirl.build(:user, :tasks => [FactoryGirl.build(:task)]).should be_valid
   end
 
   it "with content absence" do
-    FactoryGirl.build(:task, :user => FactoryGirl.create(:user), :content => "").should_not be_valid
+    FactoryGirl.build(:user, :tasks => [FactoryGirl.build(:task, :content => "")]).should_not be_valid
   end
 
   it "check without user existence" do
@@ -24,12 +24,12 @@ describe Task do
   end
 
   it "check with user existence" do
-    task = FactoryGirl.build(:task, :user => FactoryGirl.create(:user))
+    task = FactoryGirl.create(:user, :tasks => [FactoryGirl.build(:task)]).tasks.first
     task.check_user_existence.should eq(nil)
   end
 
   it "check with task scope to destroy" do
-    task = FactoryGirl.create(:task, :user => FactoryGirl.create(:user))
+    task = FactoryGirl.build(:user, :tasks => [FactoryGirl.build(:task)]).tasks.first
     task.valid_if_user_task.should eq(false)
   end
 
